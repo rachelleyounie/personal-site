77492b9640dfd0b82708ff0e8edaf86efade6756
Rachelle Younie <rachelleyounie@MacBook-Pro.ht.home>
2023-12-04 13:39:11 -0800

    init commit

---
+++ routes.ts
@@ -0,0 +1,28 @@
+const routes = [
+    {
+        title: 'about',
+        href: '/about'
+    },
+    {
+        title: 'writing',
+        href: '/writing'
+    },
+    {
+        title: 'directing',
+        href: '/directing'
+    },
+    {
+        title: 'podcast',
+        href: '/podcast'
+    },
+    {
+        title: 'poems',
+        href: '/poems'
+    },
+    {
+        title: 'contact',
+        href: '/contact'
+    },
+]
+
+export default routes
